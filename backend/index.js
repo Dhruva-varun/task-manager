8d8b0aab074fb738be8437e07286fabdfbff967a
Varun T <varunvarun60954@gmail.com>
2025-02-06 19:52:48 +0530

    Initial commit - Full project setup

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("api running");
+});
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+  console.log("Connected to Mongodb");
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
+})
+.catch((err) => console.log("MongoDb connection Error: ", err));
